b75864c17720ed671ec1a3b8921034912cd63b83
Charlie Andrews <cwndrws@github.com>
2022-01-07 11:35:22 -0500

    Remove list chars since they were bugged and I don't even use them

--- lua/cwndrws_settings.lua
+++ lua/cwndrws_settings.lua
@@ -8,7 +8,6 @@ local function setup()
     vim.o.hlsearch = true
     vim.o.ignorecase = true
     vim.o.smartcase = true
-    vim.o.listchars="tab:▸\ ,eol:¬,space:·"
     vim.g.ale_fixers = ['remove_trailing_lines', 'trim_whitespace']
     vim.g.ale_fix_on_save = 1
 end
