c0dca0c073a9ef1fa1fdafd6929cf6947dfbcd3e
Charlie Andrews <cwndrws@github.com>
2022-01-07 11:29:58 -0500

    Add more settings to neovim settings

--- lua/cwndrws_settings.lua
+++ lua/cwndrws_settings.lua
@@ -1,5 +1,15 @@
 local function setup()
     vim.api.nvim_command('colorscheme gruvbox')
+    vim.o.number = true
+    vim.o.relativenumber = true
+    vim.o.mapleader = "\<Space>"
+    vim.o.incsearch = true
+    vim.o.hlsearch = true
+    vim.o.ignorecase = true
+    vim.o.smartcase = true
+    vim.o.listchars="tab:▸\ ,eol:¬,space:·"
+    vim.g.ale_fixers = ['remove_trailing_lines', 'trim_whitespace']
+    vim.g.ale_fix_on_save = 1
 end
 
 return {
