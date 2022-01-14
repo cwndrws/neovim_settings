b06f0ead4b484dad00bd1d1b4048b3e120b7eaea
Charlie Andrews <cwndrws@github.com>
2022-01-14 10:11:53 -0500

    Setup deoplete

--- lua/settings.lua
+++ lua/settings.lua
@@ -12,3 +12,4 @@ vim.o.mouse = "a"
 vim.g.ale_fixers = {'remove_trailing_lines', 'trim_whitespace'}
 vim.g.ale_fix_on_save = 1
 vim.g.nvim_tree_quit_on_open = 1
+vim.g.deoplete.enable_at_startup = 1
