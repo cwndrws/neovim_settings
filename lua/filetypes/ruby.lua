47bdfae6600534ecd1a7c0ada79fbd2181417663
Charlie Andrews <cwndrws@github.com>
2022-01-20 16:14:38 -0500

    Add ruby filetype def

--- lua/filetypes/ruby.lua
+++ lua/filetypes/ruby.lua
@@ -1,12 +1,12 @@
 local FileType = require('filetypes.filetype')
-local lua = FileType:new{
-  extensions = {"lua"},
+local ruby = FileType:new{
+  extensions = {"rb", "erb"},
   commands = {
-    "let b:ale_fixers = ['lua-format']",
+    "let b:ale_fixers = ['rubocop']",
     function ()
       vim.bo.tabstop=2
       vim.bo.shiftwidth=2
     end,
   }
 }
-return lua
+return ruby
