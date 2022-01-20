ce57c09d924b1d1d644e8b1085be42a416bd70bc
Charlie Andrews <cwndrws@github.com>
2022-01-20 15:47:00 -0500

    Fix all-file ale fixers

--- lua/settings.lua
+++ lua/settings.lua
@@ -9,7 +9,7 @@ vim.o.tabstop = 4
 vim.o.shiftwidth = 4
 vim.o.expandtab = true
 vim.o.mouse = "a"
-vim.g.ale_fixers = {'remove_trailing_lines', 'trim_whitespace'}
+vim.g.ale_fixers = {['*'] = {'remove_trailing_lines', 'trim_whitespace'}}
 vim.g.ale_fix_on_save = 1
 vim.g.ale_completion_enabled = 1
 vim.g.nvim_tree_quit_on_open = 1
