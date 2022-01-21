ada334d60be03c874bdba7608936948fd9d65f51
Charlie Andrews <cwndrws@github.com>
2022-01-21 09:46:58 -0500

    Setup focus and blur autocmds

--- lua/appearance.lua
+++ lua/appearance.lua
@@ -7,12 +7,49 @@ local function colorcolumns ()
   return "+" .. table.concat(colorcolumns, ",+")
 end
 
+local function on_focus ()
+  vim.wo.winhighlight = ''
+end
+
+local function on_blur ()
+  local winhighlight_blurred = table.concat({
+    'CursorLineNr:LineNr',
+    'EndOfBuffer:ColorColumn',
+    'IncSearch:ColorColumn',
+    'Normal:ColorColumn',
+    'NormalNC:ColorColumn',
+    'Search:ColorColumn',
+    'SignColumn:ColorColumn'
+  }, ',')
+
+  vim.wo.winhighlight = winhighlight_blurred
+end
+
 local function setup()
   vim.api.nvim_command('colorscheme gruvbox')
   vim.api.nvim_command('set cursorline')
   vim.wo.colorcolumn = colorcolumns()
   vim.highlight.link('LineNr', 'ColorColumn', {force = true})
   vim.highlight.link('CursorLine', 'ColorColumn', {force = true})
+  autocmd = require('autocmd')
+  autocmd.augroup {
+    group = 'on_focus_commands',
+    autocmds = {
+      {event = 'FocusGained', pattern = '*', cmd = on_focus},
+      {event = 'BufEnter', pattern = '*', cmd = on_focus},
+      {event = 'VimEnter', pattern = '*', cmd = on_focus},
+      {event = 'WinEnter', pattern = '*', cmd = on_focus},
+    }
+  }
+
+  autocmd.augroup {
+    group = 'on_blur_commands',
+    autocmds = {
+      {event = 'FocusLost', pattern = '*', cmd = on_blur},
+      {event = 'BufLeave', pattern = '*', cmd = on_blur},
+      {event = 'WinLeave', pattern = '*', cmd = on_blur},
+    }
+  }
 end
 
 return {
