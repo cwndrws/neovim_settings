ab3ea6ad56e5348e59e88b95d5c6c447f5625da5
Charlie Andrews <cwndrws@github.com>
2022-01-21 09:15:33 -0500

    Change function order for appearance to fix lua error

--- lua/appearance.lua
+++ lua/appearance.lua
@@ -1,17 +1,17 @@
-local function setup()
-  vim.api.nvim_command('colorscheme gruvbox')
-  vim.wo.colorcolumn = colorcolumns()
-  vim.highlight.link('LineNr', 'ColorColumn')
-  vim.highlight.link('CursorLine', 'ColorColumn')
-end
-
 local function colorcolumns ()
-  colorcolumns = {}
+  local colorcolumns = {}
   for i=1, 250 do
     table.insert(colorcolumns, tostring(i))
   end
 
   return "+" .. table.concat(colorcolumns, ",+")
+end
+
+local function setup()
+  vim.api.nvim_command('colorscheme gruvbox')
+  vim.wo.colorcolumn = colorcolumns()
+  vim.highlight.link('LineNr', 'ColorColumn')
+  vim.highlight.link('CursorLine', 'ColorColumn')
 end
 
 return {
