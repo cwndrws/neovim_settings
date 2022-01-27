6423e40df3fcd05a659fab921a18602723551272
Charlie Andrews <cwndrws@github.com>
2022-01-27 17:23:25 -0500

    Add lsp saga config and some mappings

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,7 +1,19 @@
-local function setup ()
+local function setup_servers ()
   require'lspconfig'.rls.setup{}
   require'lspconfig'.rnix.setup{}
   require'lspconfig'.solargraph.setup{}
+end
+
+local function setup_lspsaga ()
+  local saga = require 'lspsaga'
+  saga.init_lsp_saga {
+    use_saga_diagnostic_sign = true,
+  }
+end
+
+local function setup ()
+  setup_servers()
+  setup_lspsaga()
 end
 
 return {
