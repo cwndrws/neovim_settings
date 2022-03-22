70e4d0d07917255edce0fad70b56e26c4ba5bf09
Charlie Andrews <charlie@cwndr.ws>
2022-03-21 20:07:19 -0400

    Update rg command for fzf

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -42,18 +42,9 @@ local function setup ()
   -- <Leader>b -- fuzzy finder for open buffers
   nmap('<Leader>b', ':Buffers <CR>')
 
-
-  -- --column: Show column number
-  -- --line-number: Show line number
-  -- --no-heading: Do not show file headings in results
-  -- --fixed-strings: Search term as a literal string
-  -- --ignore-case: Case insensitive search
-  -- --no-ignore: Do not respect .gitignore, etc...
-  -- --hidden: Search hidden files and folders
-  -- --follow: Follow symlinks
-  -- --glob: Additional conditions for search (in this case ignore everything in the .git/ folder)
-  -- --color: Search color options
-  vim.api.nvim_command('command! -bang -nargs=* Find call fzf#vim#grep(\'rg --column --line-number --no-heading --fixed-strings --ignore-case --no-ignore --hidden --follow --glob "!.git/*" --color "always" \'.shellescape(<q-args>), 1, <bang>0)')
+  vim.api.nvim_command('command! -bang -nargs=* Rg' +
+      ' call fzf#vim#grep("rg --column --line-number --no-heading --color=always --smart-case ".shellescape(<q-args>), 1,' +
+      ' fzf#vim#with_preview({\'options\': \'--delimiter : --nth 4..\'}), <bang>0)')
 
   -- <Leader>F -- ripgrep live fuzzy search with fzf
   nmap('<Leader>F', ':Find <CR>')
